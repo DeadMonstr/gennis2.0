35bec1c1f34eacfdc13d6047a664753c96d01eea
DeadMonstr <deedmonstr@gmail.com>
2024-07-02 14:15:26 +0500

    add new features

--- src/app/routers/AppRouter.js
+++ src/app/routers/AppRouter.js
@@ -2,6 +2,7 @@ import React, {Suspense} from 'react';
 import {createRoutesFromElements, Route, RouterProvider} from "react-router";
 import {createBrowserRouter} from "react-router-dom";
 import {Home} from "pages/home/ui/home";
+import Layout from "app/layout/Layout";
 
 export const AppRouter = () => {
 
@@ -12,6 +13,12 @@ export const AppRouter = () => {
                     path={"/"}
                     element={<Home/>}
                 />
+
+
+                <Route path={"platform"} element={<Layout/>}>
+
+                </Route>
+
 
             </>
         )
