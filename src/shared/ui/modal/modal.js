9dc2be51cb75326f115ef92dd42db29a42b0f6ab
null <raymuruvsanomuru@gmail.com>
2024-07-02 12:02:19 +0300

    test

---
+++ src/shared/ui/modal/modal.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import classNames from "classnames";
+
+import cls from "./modal.module.sass";
+import close from "shared/assets/icons/cross.svg";
+
+const Modal = ({children, active, setActive}) => {
+
+    const onClick = (target) => {
+        if (target.className.includes("outClose")) {
+            setActive(false)
+        } else if (target.className.includes("innerClose")) {
+            setActive(false)
+        }
+    }
+
+    if (active) {
+        return (
+            <div
+                className={classNames(cls.modal, "outClose")}
+                onClick={(e) => onClick(e.target)}
+            >
+                <div className={cls.modal__inner}>
+                    <img
+                        className={classNames(cls.modal__close, "innerClose")}
+                        onClick={(e) => onClick(e.target)}
+                        src={close}
+                        alt=""
+                    />
+                    {children}
+                </div>
+            </div>
+        );
+    }
+};
+
+export default Modal;
