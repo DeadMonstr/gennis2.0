30f6190e486f5395ebffe50278b512b755b20000
null <raymuruvsanomuru@gmail.com>
2024-07-03 15:20:53 +0300

    added header

---
+++ src/widgets/header/ui/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import cls from "./header.module.sass"
+
+export const Header = () => {
+    return (
+        <header className={cls.head}>
+
+        </header>
+    );
+};
