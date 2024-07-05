8aa4ce79efa062e476c705e4cc145fa98796399a
shaha <shahaunarov7@gmail.com>
2024-07-05 16:37:34 +0500

    register togirlandi

--- src/pages/register/ui/register.js
+++ src/pages/register/ui/register.js
@@ -2,124 +2,140 @@ import React, { useState } from 'react';
 import cls from "./register.module.sass";
 import Button from "shared/ui/button/button";
 import bg__img from 'shared/assets/images/reg__bg.svg'
-import {Input} from 'shared/ui/input'
-import {Textarea} from "shared/ui/textArea";
+import { Input } from 'shared/ui/input';
+import { Textarea } from "shared/ui/textArea";
 import { Select } from "../../../shared/ui/select";
-import {SearchPlatformInput} from "../../../features/searchInput";
 
-// Options for Select dropdowns
 const studentOptions = {
-    language:
-        [
-            {
-                id: 1,
-                value: "uz",
-                name: "o'zbek"
-            },
-            {
-                id: 2,
-                value: "ru",
-                name: "rus"
-            }
-        ],
-    subject:
-        [
-            {
-                id: 1,
-                value: "rus tili",
-                name: "rus tili"
-            },
-            {
-                id: 1,
-                value: "ingliz tili",
-                name: "ingliz tili"
-            },
-            {
-                id: 1,
-                value: "matematika",
-                name: "matematika"
-            }
-        ],
-    time:
-        [
-            {
-                id: 1,
-                value: "1-smena",
-                name: "1-smena"
-            },
-            {
-                id: 1,
-                value: "2-smena",
-                name: "2-smena"
-            },
-            {
-                id: 1,
-                value: "hamma vaqt",
-                name: "hamma vaqt"
-            }
-        ]
+    language: [
+        {
+            id: 1,
+            value: "uz",
+            name: "o'zbek"
+        },
+        {
+            id: 2,
+            value: "ru",
+            name: "rus"
+        }
+    ],
+    subject: [
+        {
+            id: 1,
+            value: "rus tili",
+            name: "rus tili"
+        },
+        {
+            id: 2,
+            value: "ingliz tili",
+            name: "ingliz tili"
+        },
+        {
+            id: 3,
+            value: "matematika",
+            name: "matematika"
+        }
+    ],
+    time: [
+        {
+            id: 1,
+            value: "1-smena",
+            name: "1-smena"
+        },
+        {
+            id: 2,
+            value: "2-smena",
+            name: "2-smena"
+        },
+        {
+            id: 3,
+            value: "hamma vaqt",
+            name: "hamma vaqt"
+        }
+    ]
 };
 
 const teacherOptions = {
-    language:
-        [
-            {
-                id: 1,
-                value: "uz",
-                name: "o'zbek"
-            },
-            {
-                id: 1,
-                value: "ru",
-                name: "rus"
-            }
-        ],
-    subject:
-        [
-            {
-                id: 1,
-                value: "rus tili",
-                name: "rus tili"
-            },
-            {
-                id: 1,
-                value: "ingliz tili",
-                name: "ingliz tili"
-            },
-            {
-                id: 1,
-                value: "matematika",
-                name: "matematika"
-            }
-        ]
+    language: [
+        {
+            id: 1,
+            value: "uz",
+            name: "o'zbek"
+        },
+        {
+            id: 2,
+            value: "ru",
+            name: "rus"
+        }
+    ],
+    subject: [
+        {
+            id: 1,
+            value: "rus tili",
+            name: "rus tili"
+        },
+        {
+            id: 2,
+            value: "ingliz tili",
+            name: "ingliz tili"
+        },
+        {
+            id: 3,
+            value: "matematika",
+            name: "matematika"
+        }
+    ]
 };
 
 const employerOptions = {
-    profession:
-        [
-            {
-                id: 1,
-                value: "farrosh",
-                name: "farrosh"
-            },
-            {
-                id: 1,
-                value: "elektrik",
-                name: "elektrik"
-            },
-            {
-                id: 1,
-                value: "qorovul",
-                name: "qorovul"
-            }
-        ]
+    profession: [
+        {
+            id: 1,
+            value: "farrosh",
+            name: "farrosh"
+        },
+        {
+            id: 2,
+            value: "elektrik",
+            name: "elektrik"
+        },
+        {
+            id: 3,
+            value: "qorovul",
+            name: "qorovul"
+        }
+    ]
 };
+
+const userstype = {
+    types: [
+        {
+            id: 1,
+            value: "student",
+            name: "Student"
+        },
+        {
+            id: 2,
+            value: "teacher",
+            name: "Teacher"
+        },
+        {
+            id: 3,
+            value: "employer",
+            name: "Employer"
+        }
+    ]
+}
 
 export const Register = () => {
     const [registerType, setRegisterType] = useState('student');
 
     const onChange = (value) => {
         console.log(value, "value");
+    };
+
+    const handleUserTypeChange = (value) => {
+        setRegisterType(value);
     };
 
     const renderFormFields = () => {
@@ -138,8 +154,8 @@ export const Register = () => {
                         />
                         <Input
                             placeholder="Familiya"
-                            type="text"
-                            required
+                            type="text" r
+                            equired
                         />
                         <Input
                             placeholder="Otasining ismi"
@@ -162,8 +178,7 @@ export const Register = () => {
                             required
                         />
                         <Textarea
-                            placeholder="Kommentariya"
-                        />
+                            placeholder="Kommentariya" />
                         <Select
                             defaultValue="Ta'lim tili"
                             options={studentOptions.language}
@@ -293,10 +308,8 @@ export const Register = () => {
 
     return (
         <div className={cls.login}>
-            <div>
-                <Button onClick={() => setRegisterType('student')}>Student</Button>
-                <Button onClick={() => setRegisterType('teacher')}>Teacher</Button>
-                <Button onClick={() => setRegisterType('employer')}>Employer</Button>
+            <div className={cls.selection}>
+                <Select defaultValue="User Type" options={userstype.types} onChangeOption={handleUserTypeChange} />
             </div>
             <div className={cls.login__boxes}>
                 <div className={cls.login__boxes__login__box}>
