1e6ac65e4a945ed8c0fbfed5187e5e688721a0e5
null <raymuruvsanomuru@gmail.com>
2024-07-06 11:33:11 +0300

    changed table

--- src/shared/ui/table/table.js
+++ src/shared/ui/table/table.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import classNames from "classnames";
 
 import cls from "./table.module.sass";
 
-export const Table = ({children ,extraClass}) => {
+export const Table = ({children, extraClass}) => {
     return (
-        <table className={`${cls.table} ${extraClass}`}>
+        <table className={classNames(cls.table, extraClass)}>
             {children}
         </table>
     );
