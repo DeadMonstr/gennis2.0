22ad442e64039701b68a31c341380c53b463e2c1
ikromovvv <isardor859@gmail.com>
2024-07-27 14:42:47 +0300

    change slice name1

--- src/entities/students/index.js
+++ src/entities/students/index.js
@@ -6,4 +6,7 @@ export {CreateGroup} from "./ui/createGroup/CreateGroup"
 
 export {default as newStudents} from "./model/studentsSlice"
 
+export {fetchNewStudentsData} from "./model/studentsThunk"
+export {getNewStudentsData , getStudyingStudents} from "./model/selector/studentsSelector"
+
 export {StudentsListDirector} from "./ui/studentsListDirector/studentsListDirector";
