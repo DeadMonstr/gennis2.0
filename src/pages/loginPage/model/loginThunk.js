7ea99612fcc7fc531117fbc3f9285dc4a6b778c9
ikromovvv <isardor859@gmail.com>
2024-09-04 17:14:05 +0300

    buldididdideif

--- src/pages/loginPage/model/loginThunk.js
+++ src/pages/loginPage/model/loginThunk.js
@@ -10,14 +10,14 @@ export const fetchLoginUser = createAsyncThunk(
         //     "Authorization": "Bearer " + token,
         //     'Content-Type': 'application/json'
         // }
-        return await request(`${API_URL}Api/token/`, "POST", JSON.stringify(data))
+        return await request(`${API_URL}token/`, "POST", JSON.stringify(data))
     })
 
 export const userRefreshData = createAsyncThunk(
     "loginSlice/userRefreshData",
     async (refresh) => {
         const {request} = useHttp()
-        return await request(`${API_URL}Api/token/refresh/`, "POST", JSON.stringify(refresh))
+        return await request(`${API_URL}token/refresh/`, "POST", JSON.stringify(refresh))
     }
 )
 
