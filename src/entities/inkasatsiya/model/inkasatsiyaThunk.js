34bdee8deaeb7c1cb2a121f57609c5dc270e9218
ikromovvv <isardor859@gmail.com>
2024-09-10 03:37:53 +0300

    dasdsa

--- src/entities/inkasatsiya/model/inkasatsiyaThunk.js
+++ src/entities/inkasatsiya/model/inkasatsiyaThunk.js
@@ -4,8 +4,8 @@ import {API_URL, headers, useHttp} from "../../../shared/api/base";
 
 export const inkasatsiyaThunk = createAsyncThunk(
     "inkasatsiyaSlice/inkasatsiyaThunk",
-    async (res) => {
+    async ({res , branchId}) => {
         const {request} = useHttp()
-        return await request(`${API_URL}Encashment/encashment/`, "POST", JSON.stringify(res), headers())
+        return await request(`${API_URL}Encashment/encashment/?branch=${branchId}/`, "POST", JSON.stringify(res), headers())
     }
 )
