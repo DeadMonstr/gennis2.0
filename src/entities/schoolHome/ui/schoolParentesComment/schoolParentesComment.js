ff75f9bad1f60164f7401444be3cfe8098ee629d
ikromovvv <isardor859@gmail.com>
2024-09-24 14:57:47 +0300

    dadas

--- src/entities/schoolHome/ui/schoolParentesComment/schoolParentesComment.js
+++ src/entities/schoolHome/ui/schoolParentesComment/schoolParentesComment.js
@@ -6,6 +6,8 @@ import starImg from "shared/assets/icons/mingcute_star-fill.svg"
 import React from "react";
 import Slider from "react-slick";
 
+import idea from "../../../../shared/assets/icons/turonIdea.png";
+
 const data = [
     {
         descr: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged...",
@@ -46,6 +48,26 @@ const data = [
 ]
 const render = () => {
     return data.map((item, i) => (
+
+        // <div>
+        //     <img
+        //         className={cls.items__image}
+        //         src={idea}
+        //         alt=""
+        //     />
+        //     <h2 className={cls.items__title}>{item.descr}</h2>
+        //     <p className={cls.items__more}>
+        //         <div className={cls.box_commentStar}>
+        //             {
+        //                 item?.star.map(() =>
+        //                     (
+        //                         <img src={starImg} alt=""/>
+        //                     )
+        //                 )
+        //             }
+        //         </div>
+        //     </p>
+        // </div>
         <div className={cls.box_main}>
             <div className={cls.box}>
                 <div className={cls.topIcon}>
@@ -72,69 +94,29 @@ const render = () => {
 export const SchoolParentesComment = () => {
 
     const settings = {
-        infinite: true,
+        infinite: false,
         dots: true,
-        speed: 500,
+        speed: 900,
+
+
         slidesToShow: 3,
         slidesToScroll: 1,
+        className: cls.homeSlider,
+        arrows: false
 
-        // className: cls.boxes,
-        // nextArrow: <SampleNextArrow/>,
-        // prevArrow: <SamplePrevArrow/>,
-        responsive: [
-            {
-                breakpoint: 1350,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
+    }
 
-                }
-            },
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-
-                }
-            },
-            {
-                breakpoint: 830,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-
-                }
-            },
-            {
-                breakpoint: 650,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-
-                }
-            },
-            {
-                breakpoint: 500,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-    };
     return (
         <div className={cls.comment}>
             <div className={cls.comment_title}>
                 Parent's <span>comment</span>
             </div>
             <div className={cls.boxes}>
-
+                <Slider {...settings}>
                     {render()}
-
+                </Slider>
             </div>
         </div>
-
 
 
     );
