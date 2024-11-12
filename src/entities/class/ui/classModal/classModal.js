ff8614b9c26d14d5b001c78bbfa728280b6e35fa
null <raymuruvsanomuru@gmail.com>
2024-11-12 19:25:34 +0500

    dsdsad

--- src/entities/class/ui/classModal/classModal.js
+++ src/entities/class/ui/classModal/classModal.js
@@ -110,6 +110,8 @@ export const ClassModal = ({
     }, [selectedSubject])
 
 
+
+
     const onChangeTotalHours = (name, hours) => {
         setTotalHours(prev => prev.map(item => {
             // console.log(item, "item")
