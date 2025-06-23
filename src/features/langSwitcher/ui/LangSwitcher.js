f3a31297a31070bf7e7cd21bfc8e9694be98d684
DeadMonstr <deedmonstr@gmail.com>
2025-06-23 13:11:41 +0500

    fixed changes

--- src/features/langSwitcher/ui/LangSwitcher.js
+++ src/features/langSwitcher/ui/LangSwitcher.js
@@ -1,22 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 
 import cls from "./langSwitcher.module.sass"
 
 import arrowDown from "shared/assets/icons/Trailing element-selected.svg"
 import lang from "shared/assets/icons/language.svg"
-import {onChange} from "features/studentPayment/model/studentPaymentSlice";
 import {useTranslation} from "react-i18next";
+
+import UzbSvg from "shared/assets/icons/twemoji_flag-uzbekistan.svg"
+import RusSvg from "shared/assets/icons/fxemoji_russianflag.svg"
+import classNames from "classnames";
 
 
 const langs = [
     {
         value: "uz",
-        label: "Uzb"
+        label: "Uzb",
+        icon: UzbSvg
     },
     {
         value: "ru",
-        label: "Rus"
+        label: "Rus",
+        icon: RusSvg
     },
 ]
 
@@ -25,29 +30,38 @@ const LangSwitcher = () => {
 
     const {t, i18n} = useTranslation();
 
+    const [active, setActive] = useState(false)
+    const [activeLang,setActiveLang] = useState("Uzb")
+
 
     const onChangeLanguage = (type) => {
         i18n.changeLanguage(type)
+        setActive(false)
+        setActiveLang(langs.filter(item => item.value === type)[0].label)
     }
 
 
     return (
         <div className={cls.switcher}>
 
-            <div className={cls.switcher__lang}>
+            <div className={cls.switcher__lang} onClick={() => setActive(!active)}>
                 <img src={lang} alt=""/>
-                <span>Uzb</span>
+                <span>{activeLang}</span>
                 <img src={arrowDown} alt=""/>
             </div>
 
-            <div className={cls.switcher__popup}>
+            <div
+                className={classNames(cls.switcher__popup, {[cls.active]: active})}
+            >
                 {
                     langs.map(item => {
 
 
                         return (
                             <div onClick={() => onChangeLanguage(item.value)} className={cls.item}>
-                                {item.label}
+                                <img src={item.icon} alt=""/>
+                                <span>{item.label}</span>
+
                             </div>
                         )
 
