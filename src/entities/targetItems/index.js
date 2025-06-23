0aecd536d83f78d95f6a282d4853860f15dc6aa3
null <raymuruvsanomuru@gmail.com>
2025-06-23 14:44:47 +0500

    newPages

--- src/entities/targetItems/index.js
+++ src/entities/targetItems/index.js
@@ -13,7 +13,6 @@ export {TargetChildren} from "./ui/children/TargetChildren"
 
 
 
-export {TargetItemsReg} from "./ui/targetItemsReg";
 export {TargetItemsHome} from "./ui/targetItemsHome/targetItemsHome";
 export {TargetItemsSecond} from "./ui/targetItemsSecond/targetItemsSecond";
 export {TargetItemsLast} from "./ui/targetItemsLast/targetItemsLast";
--- src/pages/target/ui/Target.js
+++ src/pages/target/ui/Target.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Route, Routes} from "react-router";
-import {TargetItemsReg, TargetItemsHome, TargetItemsSecond, TargetItemsLast} from "entities/targetItems";
+import {TargetItemsHome, TargetItemsSecond, TargetItemsLast} from "entities/targetItems";
 
 const Target = () => {
     return (
@@ -8,7 +8,7 @@ const Target = () => {
             <Route path={"form"} element={<TargetItemsHome/>}/>
             <Route path={"second"} element={<TargetItemsSecond/>}/>
             <Route path={"last"} element={<TargetItemsLast/>}/>
-            <Route path={"/form/:type"} element={<TargetItemsReg/>}/>
+            {/*<Route path={"/form/:type"} element={<TargetItemsReg/>}/>*/}
             {/*<Route path={"/children"} element={<>} />*/}
 
 
