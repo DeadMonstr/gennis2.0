7b25c1f7b3d5c0ddd97eecb9f405c54b1bb4a1fb
ikromovvv <isardor859@gmail.com>
2025-07-08 11:49:59 +0300

    fsdf

--- src/pages/homePage/ui/newHomeUi/newHomeUi.js
+++ src/pages/homePage/ui/newHomeUi/newHomeUi.js
@@ -23,7 +23,7 @@ export const NewHomeUi = () => {
             <div className={cls.wrapper__container}>
                 <Routes>
                     <Route path={"/"} element={<Home/>}/>
-                    <Route path={"/student_life"} element={<StudentLife/>}/>
+                    <Route path={"/student_life"} element={<NewHomeClubs/>}/>
                 </Routes>
             </div>
 
@@ -33,8 +33,7 @@ export const NewHomeUi = () => {
 
 const Home = () => {
     return (
-        <div className={cls.wrapper__container}>
-            {/* Submenu scroll targetlari */}
+        <>
             <section id="homepage">
                 <NewHomeMain/>
             </section>
@@ -44,7 +43,7 @@ const Home = () => {
             </section>
 
             <section id="slider">
-                <div >  <NewHomeDailyLife/></div>
+                <div><NewHomeDailyLife/></div>
             </section>
 
 
@@ -52,32 +51,8 @@ const Home = () => {
             <section id="quickLinks">
                 <NewHomeAdmissions/>
             </section>
-        </div>
+        </>
     )
 }
 
 
-const StudentLife = () => {
-    return (
-        <div className={cls.wrapper__container}>
-            {/* Submenu scroll targetlari */}
-                <NewHomeClubs/>
-            {/*<section id="sports_and_arts">*/}
-            {/*    <div ><NewHomePrincipal/></div>*/}
-            {/*</section>*/}
-
-            {/*<section id="school_trips">*/}
-            {/*    <div >  <NewHomeDailyLife/></div>*/}
-            {/*</section>*/}
-
-
-            {/*/!* Admissions (ariza) *!/*/}
-            {/*<section id="competitions">*/}
-            {/*    <NewHomeAdmissions/>*/}
-            {/*</section>*/}
-            {/*<section id="student_council">*/}
-            {/*    <NewHomeAdmissions/>*/}
-            {/*</section>*/}
-        </div>
-    )
-}
