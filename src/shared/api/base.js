b42067f40c328c58693c3406543b065e3fecc941
ikromovvv <isardor859@gmail.com>
2025-07-12 16:05:33 +0300

    fsd

--- src/shared/api/base.js
+++ src/shared/api/base.js
@@ -1,7 +1,7 @@
 //
 // export const API_URL_DOC = `http://192.168.1.61:8000/`
-export const API_URL_DOC = `http://26.253.30.50:8000/`
-// export const API_URL_DOC = `http://26.12.122.72:7622/`
+// export const API_URL_DOC = `http://26.253.30.50:8000/`
+export const API_URL_DOC = `http://26.12.122.72:7622/`
 //
 // export const API_URL_DOC = `https://school.gennis.uz/`
 export const API_URL = `${API_URL_DOC}api/`
